caf573cab19ace5f5c05a4f94246188188d9ab40
luoxiaoran <1439221309@qq.com>
2021-04-19 08:26:49 +0800

    '管理员修改-2'

--- config.js
+++ config.js
@@ -3,20 +3,12 @@
 //封装api模块  请求接口的模块
 let BASE_URL = ''
 if (process.env.NODE_ENV == 'development') {
-<<<<<<< HEAD
 //http://8.129.51.225:8080/YouGuang_war
     BASE_URL = 'http://8.129.51.225:8080/YouGuang_war' // qe环境
 } else {
     BASE_URL = 'http://8.129.51.225:8080/YouGuang_war' // 生产环境
-=======
 
-    BASE_URL = 'http://8.129.51.225:8080/YouGuang_war' // qe环境
-} else {
-    BASE_URL = 'http://8.129.51.225:8080/YouGuang_war' // 生产环境
-	// http://8.129.51.225:8080/YouGuang_war
-	// http://youguang.cn.utools.club
->>>>>>> e387507779c4dac417f75c8beed056bc076c7cb6
-}
+} 
 const config = {
 	base_url: BASE_URL,
 	appId: 'wxbd675765e5ceed38',
